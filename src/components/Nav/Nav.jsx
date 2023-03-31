4bed089f71bd8f8efe52dc28869724e6ff7af2d0
shojol021 <shojol021@gmail.com>
2023-04-01 01:03:34 +0600

    json file created

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,6 +6,14 @@ const Nav = () => {
             <div className="flex-1">
                 <a className="btn btn-ghost normal-case text-xl text-4xl">Knowledge Cafe</a>
             </div>
+
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Item 1</a></li>
+                    <li><a>Item 2</a></li>
+                </ul>
+            </div>
+
             <div className="flex-none gap-2">
                 <div className="form-control">
                     <input type="text" placeholder="Search" className="input input-bordered" />
