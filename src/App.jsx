5441c2220a83f20af54a770ea6aad768232825a2
shojol021 <shojol021@gmail.com>
2023-03-31 21:53:45 +0600

    added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
+import Nav from './components/Nav/Nav';
 
 const App = () => {
   return (
     <div>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+        <Nav></Nav>
     </div>
   );
 };
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl text-4xl">Knowledge Cafe</a>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="form-control">
+                    <input type="text" placeholder="Search" className="input input-bordered" />
+                </div>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="../../../public/user-icon.jpg" />
+                        </div>
+                    </label>
+                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
